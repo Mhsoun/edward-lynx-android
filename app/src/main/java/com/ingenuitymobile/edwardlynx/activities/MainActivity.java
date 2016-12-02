8c72e7729089eb75f192b2290b1025a05aa1d01c
Joseph Jorge Repaso <jorge@ingenuity.ph>
2016-12-02 14:59:27 +0800

    Added packages for activities, fragments, services and views

--- app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
+++ app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ingenuitymobile.edwardlynx;
+package com.ingenuitymobile.edwardlynx.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ingenuitymobile.edwardlynx.R;
 
 public class MainActivity extends AppCompatActivity {
 
