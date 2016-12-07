6332c0a2b8ff8278e4a8f6a49ec40f73a927e853
Joseph Jorge Repaso <jorge@ingenuity.ph>
2016-12-07 11:48:26 +0800

    Added Fabrics library

--- app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
+++ app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
@@ -3,13 +3,18 @@ package com.ingenuitymobile.edwardlynx.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
 import com.ingenuitymobile.edwardlynx.R;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Fabric.with(this, new Crashlytics());
     setContentView(R.layout.activity_main);
   }
 }
+
