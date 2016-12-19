9d189988338ae631f4d2c4c31858b863f900fad8
Joseph Jorge Repaso <jorge@ingenuity.ph>
2016-12-19 18:01:00 +0800

    Added dashboard UI. Added font style and theming

--- app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
+++ app/src/main/java/com/ingenuitymobile/edwardlynx/activities/MainActivity.java
@@ -1,17 +1,13 @@
 package com.ingenuitymobile.edwardlynx.activities;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
 import com.ingenuitymobile.edwardlynx.R;
 
 import io.fabric.sdk.android.Fabric;
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO: Move this to where you establish a user session
     logUser();
-  }
-
-  @Override
-  protected void attachBaseContext(Context newBase) {
-    super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
-  }
-
-  public void forceCrash(View view) {
-    throw new RuntimeException("This is a crash");
   }
 
   private void logUser() {
